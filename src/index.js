69bcf13b9bc35a8befc119c20f19243738db8e4d
Paavveel <pa.tomilin@gmail.com>
2023-02-07 13:23:44 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBrackets = [];
+  const closeBrackets = [];
+
+  for (const c of bracketsConfig) {
+    openBrackets.push(c[0]);
+    closeBrackets.push(c[1]);
+  }
+
+  const stack = [];
+
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  for (const char of str) {
+    if (closeBrackets.includes(char)) {
+      if (openBrackets.indexOf(stack.at(-1)) === closeBrackets.indexOf(char)) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
+};
